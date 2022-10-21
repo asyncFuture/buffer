d8eedbbacbdaf81b17a6561433c0858700a22639
dulag <tilmaart451@gmail.com>
2022-10-21 23:53:34 +0200

    add better example

---
+++ src/test/java/eu/dulag/test/Test.java
@@ -0,0 +1,29 @@
+package eu.dulag.test;
+
+import eu.dulag.buffer.Buffer;
+import eu.dulag.buffer.ByteBuf;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Buffer heap = ByteBuf.heap();
+        heap.writeInt(2022);
+
+        heap.detach();
+
+        System.out.println(Arrays.toString(heap.array()));
+        System.out.println("available heap buffers: " + ByteBuf.free(false));
+
+        System.out.println(heap + " free=" + ByteBuf.isFree(heap));
+
+        heap.writeInt(2022);
+
+        System.out.println(Arrays.toString(heap.array()));
+        heap.detach();
+        System.out.println(heap + " was detached");
+
+        System.out.println("available heap buffers: " + ByteBuf.free(false));
+    }
+}
