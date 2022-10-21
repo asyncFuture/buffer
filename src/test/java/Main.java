880cbabd48e5314b7e42d16ca4f676b37334a540
dulag <tilmaart451@gmail.com>
2022-10-21 23:53:09 +0200

    delete Main.java

--- src/test/java/Main.java
+++
@@ -1,9 +0,0 @@
-import eu.dulag.buffer.Buffer;
-import eu.dulag.buffer.ByteBuf;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Buffer buf = ByteBuf.heap();
-    }
-}
